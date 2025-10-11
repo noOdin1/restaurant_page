0c878320ab2aea28f46677f2cdff0d414bf491ab
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-11 22:57:49 +0800

    Add the images and descript text for each of menu items

--- src/menu.page.js
+++ src/menu.page.js
@@ -27,4 +27,39 @@ function loadMenuPage() {
     "Here is a sample of the scrumptous food that we have to offer: ",
   );
 
+  let firstImg = createElement("img", ["content", "image"], "firstImg");
+  firstImg.src = lasagnaPic;
+  let itemDesc1 = createElement(
+    "p",
+    ["content", "menuDescription"],
+    "firstDesc",
+    "Come and sample our rich Italian baked Lasagna Bolognese pasta dish, mixed with egg,  and slow-cooked meat sauce (ragù alla Bolognese), and creamy béchamel sauce, all topped with Parmigiano-Reggiano cheese and baked until bubbly and golden.",
+  );
+
+  let secondImg = createElement("img", ["content", "image"], "secondImg");
+  secondImg.src = sweetSourChixPic;
+  let itemDesc2 = createElement(
+    "p",
+    ["content", "menuDescription"],
+    "secondDesc",
+    "Another one of our amazing offering is the crispy deep fried chicken dipped in sweet and sour sauce. The golden brown sauce bell peppers, vegetables and pineapple chunks will wake your senses with a zesty tingle and one taste you with an unforgettable sensation.",
+  );
+
+  let thirdImg = createElement("img", ["content", "image"], "thirdImg");
+  thirdImg.src = steakPic;
+  let itemDesc3 = createElement(
+    "p",
+    ["content", "menuDescription"],
+    "thirdDesc",
+    "Our farm fresh steak is juicy, full of delicious moisture and as you cut into the steak, the free flowing juices just makes every piece even more delicious. Each bite of that tenderness and taste will you of the beef's freshness.",
+  );
+
+  let fourthImg = createElement("img", ["content", "image"], "fourthImg");
+  fourthImg.src = spicyLambCurryPic;
+  let itemDesc4 = createElement(
+    "p",
+    ["content", "menuDescription"],
+    "fourthDesc",
+    "Our flavorful Asian dish of lamb curry, slow-cooked stew of tender lamb in a rich, aromatic sauce made with our very own secret spices and herbs, will definately satisfy your appetite.",
+  );
 }
