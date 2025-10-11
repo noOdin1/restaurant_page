b9a84e72aa492688009314cbde169564c090c71d
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-11 22:41:03 +0800

    Add images using import, for html-loader

--- src/menu.page.js
+++ src/menu.page.js
@@ -5,3 +5,9 @@ import {
   removeAllChildElemById,
 } from "./removeAllChildElem.js";
 import createElement from "./element_creator.js";
+/* import images for html use */
+import lasagnaPic from "./lasagna_193x128.jpg";
+import sweetSourChixPic from "./sweetSourChicken_192x128.jpg";
+import steakPic from "./steak.jpg";
+import spicyLambCurryPic from "./spicy_lamb_curry.jpg";
+
