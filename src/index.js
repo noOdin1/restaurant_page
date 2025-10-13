e0ab7443a98300479fbb4f509f98f24838f9d811
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-13 17:16:46 +0800

    Import each page Javascript for execution whenever a nav btn is clicked

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@
 import "./style.css";
 import "./style.menu.page.css";
 import "./style.about.page.css";
+import loadHomePage from "./home.page.js";
+import loadAboutPage from "./about.page.js";
+import loadMenuPage from "./menu.page.js";
 
 console.log("Compilation works, hello world!");
